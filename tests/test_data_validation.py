21ca32009f92feb8f7200cd13a0e5ab9e09d3316
prostabkundu123 <21f1001797@ds.study.iitm.ac.in>
2025-10-19 13:07:30 +0000

    Second commit to dev branch. Pytest coode changes and second iteration with 300 rows of iris data

--- tests/test_data_validation.py
+++ tests/test_data_validation.py
@@ -11,3 +11,9 @@ def test_data_integrity():
     expected_cols = {"sepal_length", "sepal_width", "petal_length", "petal_width", "species"}
     assert expected_cols.issubset(df.columns)
 
+def test_numeric_values_positive():
+    df = pd.read_csv("data/iris.csv")
+    numeric_cols = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+    # 4. Check all numeric values are positive
+    for col in numeric_cols:
+        assert (df[col] > 0).all(), f"Negative or zero values found in {col}"
